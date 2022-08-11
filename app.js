919701c2aa810114b93c3158246b5bbea3e21b74
Indra Faozi <indrafaozi00@gmail.com>
2022-08-11 13:05:50 +0700

    First commit my-portfolio

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res){
+    res.send('Hello World this is my portfolio')
+});
+
+app.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+});
