b948ee17466aea316fadbf3829aaa747a65460da
Indra Faozi <indrafaozi00@gmail.com>
2022-08-19 14:25:21 +0700

    Remove console_lo

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     // res.render('index');
     res.sendFile(__dirname + "/views/maintenance.html");
-    console.log('Ini untuk pengujian aja');
 });
 
 app.all('*', (req, res) => res.sendStatus(404))
