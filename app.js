8d36e3f9f57d2f4eb0c114cb9ea645bd21040758
Indra Faozi <indrafaozi00@gmail.com>
2022-08-19 18:09:40 +0700

    Change ejs view engine

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app = express();
 const port = 3000;
 // const expressLayouts = require('express-ejs-layouts');
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 // app.use(expressLayouts)
 
 app.use('/public', express.static('public'))
 
 
 app.get('/', (req, res) => {
-    res.render('index');
-    // res.sendFile(__dirname + "/views/maintenance.html");
+    // res.render('index');
+    res.sendFile(__dirname + "/views/maintenance.html");
 });
 
 app.all('*', (req, res) => res.sendStatus(404))
